faa701aa32d73f11a283374d5eaed2b4fb41dd61
FORM205\Formation <heloise.verhasselt@gmail.com>
2018-02-13 17:15:39 +0100

    Initialisation du repo de test JPetStore

---
+++ src/main/java/org/autom/testJpetstore/App.java
@@ -0,0 +1,13 @@
+package org.autom.testJpetstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/autom/testJpetstore/TestStore.java
@@ -0,0 +1,46 @@
+package org.autom.testJpetstore;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestStore {
+	
+	WebDriver driver;
+	
+	@Before
+	public void setup() {
+		if(System.getProperty("navigateur").equalsIgnoreCase("firefox")){
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Formation\\OWASP ZAP\\webdriver\\windows\\64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://localhost:8090/jpetstore/");
+		}
+		else if(System.getProperty("navigateur").equalsIgnoreCase("chrome")){
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Formation\\Downloads\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://localhost:8090/jpetstore/");
+		}
+		
+	}
+	
+	@Test
+	public void test() {
+		driver.findElement(By.linkText("Enter the Store")).click();
+		WebElement image = driver.findElement(By.id("MainImageContent"));
+		image.isDisplayed();
+		
+	}
+	
+	@After
+	public void teardown() {
+		driver.quit();
+	}
+
+}
