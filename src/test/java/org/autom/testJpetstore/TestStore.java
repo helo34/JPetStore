cf9779d31a929b880c15f0764edb179e4eb84e3f
FORM205\Formation <heloise.verhasselt@gmail.com>
2018-02-14 11:00:00 +0100

    Modif pour firefox

--- src/test/java/org/autom/testJpetstore/TestStore.java
+++ src/test/java/org/autom/testJpetstore/TestStore.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxOptions;
+//import org.openqa.selenium.firefox.FirefoxProfile;
 
 public class TestStore {
 	
@@ -16,7 +18,11 @@ public class TestStore {
 	@Before
 	public void setup() {
 		if(System.getProperty("navigateur").equalsIgnoreCase("firefox")){
+			// commentaires à décommenter si problème avec firefox
+		//FirefoxOptions options = new FirefoxOptions().setProfile(new FirefoxProfile());
+		//options.addPreference("browser.tabs.remote.autostart", false);
 		System.setProperty("webdriver.gecko.driver","C:\\Users\\Formation\\OWASP ZAP\\webdriver\\windows\\64\\geckodriver.exe");
+		//driver = new FirefoxDriver(options);
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get("http://localhost:8090/jpetstore/");
